b62a3be611a1ca22bfe8b6f4687fdd7c33259ba4
Deepu <itsdeeputiwari@gmail.com>
2025-10-05 17:21:43 +0530

    add rabbitmq

--- authors/src/index.js
+++ authors/src/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const dotenv = require("dotenv");
 const {connectDB} =require("./configs/db")
 const authorRoutes=require("./routes/routes");
+const {connectRabbitMq,publicToQueue,invalidateCacheJob}=require("../src/utils/rabbitmQ");
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+connectRabbitMq();
 const PORT = process.env.NODE_PORT || 5000;
 app.use("/api/v1",authorRoutes)
 connectDB().then(()=>{
---
+++ authors/src/utils/rabbitmQ.js
@@ -0,0 +1,42 @@
+const amqp = require("amqplib");
+
+let channel;
+const connectRabbitMq = async () => {
+    try {
+        const connection = await amqp.connect({
+            protocol: "amqp",
+            hostname: "localhost",
+            port: 5672,
+            username: "guest",
+            password: "guest"
+        });
+
+        channel = await connection.createChannel();
+        console.log("Connected successfully to RabbitMQ");
+    } catch (error) {
+        console.error("Failed to connect to RabbitMQ:", error);
+    }
+};
+const publicToQueue=async(queuName,message)=>{
+    if(!channel){
+        return console.error("RabbitMQ is not initialized");
+    }
+    await channel.assertQueue(queuName,{durable:true});
+    channel.sendToQueue(queuName, Buffer.from(JSON.stringify(message)),{
+        persistent:true,
+    });
+}
+const invalidateCacheJob=async(cacheKeys)=>{
+    try {
+        const message={
+            action:"invalidateCache",
+            key:cacheKeys
+        };
+        await publicToQueue("cache-invalidation", message);
+        console.log("Cache invalidation job published to Rabbit");
+    } catch (error) {
+        console.error("Failed to connect to RabbitMQ:", error); 
+    }
+}
+// Call the function
+module.exports={connectRabbitMq,publicToQueue,invalidateCacheJob};
